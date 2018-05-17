31177cdafbcb2599abd4278925b4f99757c4d2df
Partha Boocha <Partha@Boocha>
2018-05-16 22:54:22 -0700

    fixed the / route redirect

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box } from "gestalt";
-import { Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import Login from './Login';
 import Callback from './Callback';
 import AuthenticatedRoute from "./AuthenticatedRoute";
@@ -9,7 +9,7 @@ import Repos from './Repos';
 const Content = () => {
     return (
         <Box paddingX={12}>
-          <AuthenticatedRoute exact path='/' component={Repos} />
+          <Route exact path='/' render={() => <Redirect to='/repos' />} />
           <Route exact path='/login' component={Login} />
           <Route path='/callback' component={Callback} />
           <AuthenticatedRoute path='/repos' component={Repos} />
